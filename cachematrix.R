26afb2fea1a226ddc5d645bf90193be5deae5b3b
Enric Martinez <runlevel0@gmail.com>
2015-01-23 17:15:20 +0100

    added some unfinished c0d3z

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Well, this functions do a lot of different things for a living.
+# One of them even worked in a circus!!!
 
-## Write a short comment describing this function
+# This beautiful and extremely useful function (meant to revolutionise humanity as a whole)
+# Will be the root from wich the Future AI will evolve!!
+# The first one creates a matrix with random numbers in it and transpose it for no good reason
+#  it later cuts of some stuff to square it to 10x10
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # takes the matrix and 
+  # Creates an empty matrix
+  m0 <<- matrix(,nrow=0, ncol=0)
+  
+  
+  
+  # Transpose it back and forth just for the sake of it
+  x <- t(x)
+  x <- t(x)
+  # Very important fnctionality of htis program!!!
+  #print("Calling my buddy Pete for help. BRB...\n")
+  #Sys.sleep(120)
+  # Test reversibility: 
+  if (length(x[,1]!=length(x[1,])) {
+    print("This Matrix-thingy is non-reversible you d******k !!\nGo make a new one!!!")
+  } else {
+    m0 <<- solve(x)
+  }
+    
 }
 
 
-## Write a short comment describing this function
+# This function is a clear demonstration of my advanced codign zk1llz: It takes the matrix from above
+# and reverses the heck out of it, once this is done it hacks your computer and sends me the password of your 
+# WoW account!
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #This part is the most difficult one as it requires 
+  # a steady pulse and good aim with hte keyboard to write
+   if()
+        y <<- solve(x)
+  #And now to finish it, the matesterly placed Curly Brace of Doom!!      
 }
